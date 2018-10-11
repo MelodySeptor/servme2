84c745e804d8d265a94d125153442487cdcd2c3d
Polete444 <polgn1995@gmail.com>
2018-10-11 12:56:23 +0200

    Puesto el sitemap y quitado de la verificacion del captcha en el caso que sea correcto.

--- public/scripts/validateCaptcha.js
+++ public/scripts/validateCaptcha.js
@@ -2,7 +2,5 @@ validateCaptcha= function() {
 	var response = grecaptcha.getResponse();
 	if(response.length == 0){
 		alert("Captcha no verificado")
-	} else {
-		alert("Captcha verificado");
 	}
 }
