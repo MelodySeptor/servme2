53a3df98dafbd4ee15370fef26b17a6c445db7ce
Polete444 <polgn1995@gmail.com>
2018-10-11 21:52:17 +0200

    Se ha definido la logica que seguira la confirmación del registro de un usuario. Siguiente paso: implementarla.

--- private/gestorLoginRegistro/gestorRegistro.js
+++ private/gestorLoginRegistro/gestorRegistro.js
@@ -1,6 +1,15 @@
 gestorRegistro=function(req, res){
     if(req.session.datosIndex.logged){res.redirect('/')}
     else{
-        res.redirect('/inicio')
+        if(compruebaFormulario(req,res)){
+            //Añadir en base de datos
+        }
+        else{res.redirect('/registro')}
     }
+}
+
+compruebaFormulario = function(req, res){
+    correcto = false
+    //Comprobar que los campos sean correctos y que el usuario no se encuentre ya registrado.
+    return correcto
 }
