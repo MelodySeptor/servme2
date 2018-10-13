4fa597ea5d4c00ca5b1ed820c7a5fa7e1a5bb3b7
Polete444 <polgn1995@gmail.com>
2018-10-13 18:42:38 +0200

    Nuevo modulo añadido

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(helmet())
 app.set('views', __dirname + '/public/views')
 app.set('view engine', 'pug')
-var port = 80
+var port = 443
 
 //Peticion general
 app.all('/', function(req, res){
